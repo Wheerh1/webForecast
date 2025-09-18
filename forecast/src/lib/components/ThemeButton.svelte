449506b6c0e9c5978480cc5e9dc00e4728086245
wheerh1 <wheerh1@student.op.ac.nz>
2025-09-19 11:03:29 +1200

    fun button

---
+++ forecast/src/lib/components/ThemeButton.svelte
@@ -0,0 +1,24 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+
+const dispatch = createEventDispatcher();
+
+
+let {styleLight = "$lb/styleLight.css", styleDark = "$lib/styleDark.css" } = $props();
+let styleCheck = 0;
+
+function handleClick() {
+    if(styleCheck === 0){
+        dispatch('ThemeChange', {detail: '$lib/styleDark.css'});
+        styleCheck = 1;
+    }
+    else if(styleCheck === 1) {
+        dispatch('ThemeChange', {detail: '$lb/styleLight.css'});
+    }
+}
+
+
+</script>
+
+<button on:click={handleClick}>Change Theme</button>
+
