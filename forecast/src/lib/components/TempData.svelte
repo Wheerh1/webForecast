da5546f1769e91824f3d9f17ed8533f6bc259fcc
Ari <churaa1@student.op.ac.nz>
2025-09-19 11:31:15 +1200

    TempData add -- risk

---
+++ forecast/src/lib/components/TempData.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { isString } from "util";
+
+    
+    let {temperature = Number(2.2)} = $props();
+    let increment = 1
+    let char = "#"
+
+    let stringBar = ""
+    stringBar = () =>{
+        tempString = ""
+        for(i = 0; i < temperature; i+= increment){
+
+        }
+        return tempString;
+    }
+
+
+</script>
+
+<div>
+    <p>{temperature}</p>
+    <p>{stringBar}</p>
+</div>
+
+
+<style>
+    div{
+        background-color: red;
+        size: 500px;
+    }
+</style>
+
--- forecast/src/routes/+page.svelte
+++ forecast/src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<script>
-    
+<script>    
+    import TempData from "$lib/components/TempData.svelte"
 </script>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>Hello</p>
+
+<TempData temperature=0.5/>
+<TempData temperature=7.5/>
